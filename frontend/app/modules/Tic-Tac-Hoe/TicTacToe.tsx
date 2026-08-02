6e6edbf4bf56e0e11ca02193a8b6afc13af55b69
Mauro Leal <wolfhackd@gmail.com>
2026-08-01 22:20:14 -0300

    fix: join Problems

--- frontend/app/modules/Tic-Tac-Hoe/TicTacToe.tsx
+++ frontend/app/modules/Tic-Tac-Hoe/TicTacToe.tsx
@@ -15,37 +15,18 @@ export type Game = {
     O: string | null;
   };
 
-  // playerNames: {
-  //   X: string | null;
-  //   O: string | null;
-  // };
-
   moves: {
     X: number[];
     O: number[];
   }
 
-  // points: {
-  //   X: number;
-  //   O: number;
-  // };
 };
-
 
 export default function TicTacToe() {
   const {roomId} = useParams();
   const [ready,setReady] = useState(false);
   const [game, setGame] = useState<Game | null>(null);
-
-
-  // useEffect(()=>{
-  //   socket.on("game:joined", (data) => {
-  //   console.log(data.socketId);
-  //   console.log(data.symbol);
-  //   });
-  //   socket.emit("room:join", roomId);
-  //   socket.on("game:joined", (data) =>console.log(data));
-  // },[roomId]); 
+  const navigate = useNavigate();
 
    const [userId, setUserId] = useState<string>("");
     useEffect(() => {
@@ -62,24 +43,40 @@ export default function TicTacToe() {
         socket.connect();
     },[]);
 
-
   useEffect(() => {
     const handleJoined = (data: { socketId: string; symbol: "X" | "O" }) => {
         console.log("Entrou:", data);
+        localStorage.setItem("userId", data.socketId);
     };
 
     const handleReconnected = (data: { socketId: string; symbol: "X" | "O" }) => {
         console.log("Reconectou:", data);
+        localStorage.setItem("userId", data.socketId);
     };
+
+    const handleGameStart = (data: Game) =>{
+        console.log("Jogo iniciado:", data);
+        setGame(data);
+        setReady(true);
+    };
+
+    const handleGameError = (data: { message: string }) => {
+        console.log("Erro:", data.message);
+        navigate("/");
+    }
 
     socket.on("game:joined", handleJoined);
     socket.on("game:reconnected", handleReconnected);
+    socket.on("game:start", handleGameStart);
+    socket.on("game:error", handleGameError);
 
     socket.emit("room:join", roomId);
 
     return () => {
         socket.off("game:joined", handleJoined);
         socket.off("game:reconnected", handleReconnected);
+        socket.off("game:start", handleGameStart);
+        socket.off("game:error", handleGameError);  
     };
 }, [roomId]);
   
@@ -98,13 +95,13 @@ export default function TicTacToe() {
             <div className="flex justify-between items-center">
               <div className="flex flex-col">
                 {/* Devo considerar que o player pode ainda não estar em sala */}
-                <span>{game?.players.X}</span>
+                <span>Player 1</span>
                 <span className="font-bold text-blue-500">X</span>
               </div>
               <span>1:0</span>
               <div className="flex flex-col">
                 {/* Devo considerar que o player pode ainda não estar em sala */}
-                <span>{game?.players.O}</span>
+                <span>Player 2</span>
                 <span className="font-bold text-red-500">O</span>
               </div>
             </div>
